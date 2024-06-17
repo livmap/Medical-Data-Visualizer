306eed697ae84af859de876d04e20d5a05ecc5e3
Livhuwani Maphupha <maphuphaprince737@gmail.com>
2024-06-17 23:05:53 +0000

    feat: graphs

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,57 +4,57 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['overweight'] = (df['weight'] / (df['height'] / 100) ** 2 > 25).astype(int)
 
 # 3
-
+df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
+df['gluc'] = (df['gluc'] > 1).astype(int)
 
 # 4
 def draw_cat_plot():
-    # 5
-    df_cat = None
+    # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke',
+    # 'alco', 'active', and 'overweight'.
+    df_cat = pd.melt(df, id_vars=['cardio'],
+                     value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
+    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename
+    # one of the collumns for the catplot to work correctly.
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index()
+    df_cat = df_cat.rename(columns={0: 'total'})
 
-    # 6
-    df_cat = None
-    
+    # Draw the catplot with 'sns.catplot()'
+    graph = sns.catplot(data=df_cat, kind="bar", x="variable", y="total", hue="value", col="cardio")
+    fig = graph.fig
 
-    # 7
-
-
-
-    # 8
-    fig = None
-
-
-    # 9
+    # Do not modify the next two lines
     fig.savefig('catplot.png')
     return fig
 
 
 # 10
 def draw_heat_map():
-    # 11
-    df_heat = None
+    # Clean the data
+    df_heat = df[(df['ap_lo'] <= df['ap_hi']) &
+                 (df['height'] >= df['height'].quantile(0.025)) &
+                 (df['height'] <= df['height'].quantile(0.975)) &
+                 (df['weight'] >= df['weight'].quantile(0.025)) &
+                 (df['weight'] <= df['weight'].quantile(0.975))
+                 ]
 
-    # 12
-    corr = None
+    # Calculate the correlation matrix
+    corr = df_heat.corr()
 
-    # 13
-    mask = None
+    # Generate a mask for the upper triangle
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
+    # Set up the matplotlib figure
+    fig, ax = plt.subplots(figsize=(16, 9))
 
+    # Draw the heatmap with 'sns.heatmap()'
+    sns.heatmap(corr, mask=mask, square=True, linewidths=0.5, annot=True, fmt="0.1f")
 
-    # 14
-    fig, ax = None
-
-    # 15
-
-
-
-    # 16
     fig.savefig('heatmap.png')
     return fig
